30e46553d0d0d48e08030e83ad9f21396c6b75d5
Sean Martin <sean@seanmartinstudio.com>
2021-11-10 11:19:41 -0800

    commit tree 8cd8fbaecd911257f39edeb5b8c313bf3db57e94

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+//1
+var customerName = "bob"
+var bestCustomer
+const leastFavoriteCustomer = 'dave'
+
+//2
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+//3
+function setBestCustomer() {
+    //var bestCustomer;
+    return bestCustomer = "not bob";
+}
+
+//4
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+//5
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'dan';
+}
+console.log(changeLeastFavoriteCustomer());
